e74ff35104ec061bb2d13787216b9d6c0cd09d32
daniel <daniel.maksimovic01@gmail.com>
2022-11-16 13:15:40 +0100

    AbstractCard

---
+++ src/Cards/AbstractCard.java
@@ -0,0 +1,26 @@
+package Cards;
+
+public abstract class AbstractCard{
+
+
+
+
+
+}
+
+interface CardsBehaviour{
+
+}
+
+interface InterfacePointBehaviour{
+
+}
+
+interface AfterTuto{
+
+}
+
+interface ValidDice{
+
+}
+
--- src/Cards/Cards.java
+++
@@ -1,4 +0,0 @@
-package Cards;
-
-public class Cards {
-}
