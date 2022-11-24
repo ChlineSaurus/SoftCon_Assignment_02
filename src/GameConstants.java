0bdc66de50f6377f4c35e6f9f6198253d736e4c9
chris <christopher.narayanan@uzh.ch>
2022-11-24 14:48:55 +0100

    adding gameconstants necessary for win

--- src/GameConstants.java
+++ src/GameConstants.java
@@ -1,7 +1,9 @@
 public enum GameConstants {
     minAllowedPlayers(2),
     maxAllowedPlayers(4),
-    maxAllowedNameLength(20);
+    maxAllowedNameLength(20),
+    minAllowedPoints(1000),
+    maxAllowedPoints(50000);
 
     public final int definedConstant;
     GameConstants(Integer definedConstant) {
--- src/cards/AbstractCard.java
+++ src/cards/AbstractCard.java
@@ -24,6 +24,7 @@ public abstract class AbstractCard {
 
     private TurnState turnState;
 
+
     public TurnState getTurnState() {
         return turnState;
     }
