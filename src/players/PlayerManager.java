dfb56e042da1718be53f491a25a804ce612edc68
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2022-11-30 08:38:43 +0100

    manager Debug

--- src/players/PlayerManager.java
+++ src/players/PlayerManager.java
@@ -149,7 +149,13 @@ public class PlayerManager {
     }
 
     private void declareWinner() {
-
+        Player winner = players.get(1);
+        for(Player aPlayer: players) {
+            if (aPlayer.getScore() > winner.getScore()) {
+                winner = aPlayer;
+            }
+        }
+        UI.Display.declareWinner(winner.name);
         exit(0);
     }
 }
