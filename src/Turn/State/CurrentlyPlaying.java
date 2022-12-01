49249719508e620fad07643f986c56a25601da9a
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2022-12-01 11:19:34 +0100

    Still not working...

--- src/Turn/State/CurrentlyPlaying.java
+++ src/Turn/State/CurrentlyPlaying.java
@@ -1,97 +1,106 @@
 package Turn.State;
 
 import Input.TuttoInput;
-import Turn.HumanInteractionManager;
-import UI.Display;
-import dice.DiceFace;
-import dice.DiceTower;
-import exceptions.IllegalUserInputExeption;
 import Turn.Turn;
+import cards.cards.cardtypes.Fireworks;
+import dice.DiceFace;
+import exceptions.IllegalUserInputExeption;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class CurrentlyPlaying implements TurnState{
     Turn aTurn;
     private boolean interactionFinished = false;
-    private boolean tutto = false;
 
     public CurrentlyPlaying(Turn pTurn){
         assert pTurn!=null;
         this.aTurn = pTurn;
     }
-
-    public HumanInteractionManager humanInteraction = new HumanInteractionManager();
     @Override
-    public void next() throws IllegalUserInputExeption {
-
-        int i = 1;
+    public void next() {
         System.out.println("I am currently playing");
         aTurn.draw();
         if(aTurn.turnCard.isEndTurn()) {
             aTurn.temporaryScore = 0;
             aTurn.setCurrentState(new EndTurn(aTurn));
-        } else {
-            aTurn.setCurrentState(new CurrentlyPlaying(aTurn));
         }
-        ManageGame();
-        if (i ==0){
-            aTurn.setCurrentState(new Tutto(aTurn));
-        }
-        else{
-            aTurn.setCurrentState(new NoMoreValidDice(aTurn));
-        }
-    }
-
-    private void ManageGame() throws IllegalUserInputExeption {
-        Setup();
         TurnFlow();
     }
+
     //wenns funktioniert, chamer die Methode lösche und d Funktionalität in ManageGame übere tue...
     //evtl. chamer au die ganzi manage game denne lösche...
-    private void Setup() throws IllegalUserInputExeption {
-        humanInteraction.DisplayOrRoll();
-    }
-    private void TurnFlow() throws IllegalUserInputExeption {
-        boolean turnFinished = false;
-        while(!turnFinished){
-            currentPlayerDice.rollNotTakenDices();
-            if (!currentPlayerDice.notNullRoll()){
-                if (!aTurn.turnCard.isImmunity()){
-                aTurn.score = 0;
-                aTurn.pointsToDeduct = 0;
 
+    private void TurnFlow() {
+        boolean turnFinished = false;
+        while(!turnFinished) {
+            aTurn.rollNotTakenDices();
+            if (!aTurn.validDiceExist()) {
+                aTurn.displayTurn("You rolled a Null");
+                if (!aTurn.turnCard.isImmunity()) {
+                    aTurn.temporaryScore = 0;
+                    aTurn.pointsToDeduct = 0;
                 }
                 aTurn.setCurrentState(new EndTurn(aTurn));
-            }
-            //ka, ob das stimmt, chas aber nid teschte, da ich mitem Error voretäre nid z schlag chume...
-            while(!interactionFinished)
-            if(currentPlayerDice.notNullRoll()){
-                Display.displayMessage("If you want to take some of the Dice type \"Y\" if not type \"N\" ");
-                System.out.println(currentPlayerDice.showNotTakenDices());
-                char takeDice = TuttoInput.takeCharInput();
-                String takeDices = String.valueOf(takeDice);
-                if (takeDices.equals("Y")){
-                    ArrayList<DiceFace> diceToRemove = humanInteraction.ChoseDice();
-                    //currentPlayerDice.removeDice(diceToRemove);
-                    System.out.println(diceToRemove);
-                    aTurn.score += currentPlayerDice.removeDice(diceToRemove);
-                }else{
-                    interactionFinished = humanInteraction.Reroll();
+            } else {
+                takeDice();
+                if (aTurn.tuttoMade()) {
+                    aTurn.setCurrentState(new Tutto(aTurn));
+                    break;
                 }
-
-            }
-            else{
-                if(/* getImmunity*/true){
-                    interactionFinished = humanInteraction.Reroll();
-                }else{
-                    aTurn.score = 0;
+                else if (wantReroll()) {
+                    continue;
+                } else {
                     aTurn.setCurrentState(new EndTurn(aTurn));
+                    break;
                 }
             }
-            System.out.println("interaction finished");
-            break;
-
         }
+    }
+    private void takeDice() {
+        String message = "Please enter the dice you want to take";
+        while (true) {
+            aTurn.displayTurn(message);
+            try {
+                ArrayList<DiceFace> diceToRemove = TuttoInput.takeDiceListInput();
+                if (diceToRemove.size() == 0) {
+                    if (!aTurn.isDiceTakenSinceRoll()) {
+                        throw new IllegalUserInputExeption("You can't exit, you must take at least one Dice");
+                    } else if (aTurn.turnCard.getClass().equals(Fireworks.class) && aTurn.validDiceExist()) {
+                        throw new IllegalUserInputExeption("Your Card is a Firework and you" +
+                                " must take all Dice");
+                    } else {
+                        break;
+                    }
+                }
+                aTurn.temporaryScore += aTurn.removeDice(diceToRemove);
+                if (aTurn.validDiceExist()){
+                    message = "If you want to take more Dice enter the Numbers, else press Enter";
+                } else {
+                    break;
+                }
+            } catch (IllegalUserInputExeption e) {
+                message = e.getMessage();
+            }
+        }
+    }
+
+    public Boolean wantReroll() {
+        ArrayList<Character> allowedChar = new ArrayList<>();
+        allowedChar.add('E');
+        allowedChar.add('R');
+        aTurn.displayTurn("Enter to reroll the dice \"R\" and if you want to end your turn \"E\"");
+        Character nextAction;
+        while(true) {
+            try {
+                nextAction = TuttoInput.takeRestrictedCharInput(allowedChar);
+                if(nextAction.equals('E')) {
+                    aTurn.turnCard.userTriesToEndTurn();
+                }
+                break;
+            } catch (IllegalUserInputExeption e) {
+                aTurn.displayTurn(e.getMessage());
+            }
+        }
+        return nextAction.equals('R');
     }
 }
