81a6053ae7d025c05e7bdd55fcebfa76a98daa5f
chris <christopher.narayanan@uzh.ch>
2022-12-05 10:22:32 +0100

    stop is now red

--- src/cards/cards/cardtypes/Stop.java
+++ src/cards/cards/cardtypes/Stop.java
@@ -5,7 +5,7 @@ import cards.AbstractCard;
 public class Stop extends AbstractCard {
 
     public Stop(){
-        name="Stop";
+        name="\u001B[31mStop"+"\u001B[0m";
         description="Tough Luck! You have to end your turn, and your left neighbour has his turn.";
         this.endTurn=true;
     }
